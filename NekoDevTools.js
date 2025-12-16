282c2b55994c390fe497d9458ac91a49f9f571e8
be_nyantorusabu <nnya13586@gmail.com>
2025-12-16 21:33:07 +0900

    UPL

---
+++ NekoDevTools.js
@@ -0,0 +1,545 @@
+(function() {
+if (window.NDT) return;
+
+// ScratchVM
+window.NDT = {};
+if (typeof vm !== 'undefined') {
+    NDT.VM = vm;
+} else if(document.getElementById('app')) {
+    NDT.VM = Object.values(document.getElementById('app'))[0].child.updateQueue.lastEffect.deps[1].scratchGui.vm;
+} else {
+    NDT.VM = null;
+}
+if (!NDT.VM) {
+    Log('e', 'ScratchVMへのアクセスに失敗しました!\nScratchVMの仕様が変更された可能性があります');
+    NDT = undefined;
+    return;
+}
+NDT.RT = NDT.VM.runtime;
+
+
+// Info/Option
+NDT.Info = {};
+NDT.Info.Ver = '0.0.19';
+NDT.Info.Message = `ハット関係のNDTEventを追加`;
+NDT.Option = {};
+NDT.Option.DisCheck = false;
+NDT.Option.DisNDTEvent = false;
+
+
+// Obj
+NDT.Sprite = {};
+NDT.Spr = NDT.Sprite;
+NDT.Event = {};
+NDT.Eve = NDT.Event;
+NDT.Spr.Position = {};
+NDT.Spr.Pos = NDT.Spr.Position;
+NDT.Spr.Event = {};
+NDT.Spr.Eve = NDT.Spr.Event;
+NDT.Spr.Variable = {};
+NDT.Spr.Var = NDT.Spr.Variable;
+NDT.Spr.List = {};
+NDT.Variable = {};
+NDT.Var = NDT.Variable;
+NDT.List = {};
+
+
+// Reload
+NDT.Reload = function() {
+    NDT.Spr.All = NDT.VM.runtime.targets;
+    NDT.Spr.IDList = NDT.Spr.All.map(s => s.id);
+    NDT.Spr.NameList = NDT.Spr.All.map(s => s.getName());
+}
+NDT.Reload();
+NDT.RT.on('PROJECT_LOADED', () => {
+    NDT.Reload();
+});
+NDT.VM.addListener('targetsUpdate', (data) => {
+    NDT.Reload();
+});
+
+
+// NDTEvent
+NDT.SC = {};
+
+class NDTEvent extends Event {
+	constructor(EveID, Args = {}) {
+		super(EveID);
+        for(const [k, v] of Object.entries(Args)) {
+            this[k] = v;
+        }
+	}
+}
+
+NDT.NDTEvent = {};
+NDT.NEve = NDT.NDTEvent;
+const Eve = new EventTarget();
+NDT.NDTEvent.Add = function(name, handler) {
+    Eve.addEventListener(name, handler);
+};
+NDT.NDTEvent.Remove = function(name, handler) {
+    Eve.removeEventListener(name, handler);
+};
+NDT.NDTEvent.Dispatch = function(EveID, Args = {}) {
+    if (NDT.Option.DisNDTEvent) return;
+    Eve.dispatchEvent(new NDTEvent(EveID, Args));
+};
+
+function NDTValiable() {
+    if (!NDT.Spr.NameList.includes('NDT')) return;
+	const Spr = NDT.Spr.Get('NDT');
+	const List = NDT.Spr.Var.NameList('NDT');
+	const Vars = {
+		Active: 1,
+		Ver: NDT.Info.Ver,
+		Update: NDT.Info.Message,
+	}
+	for (const [VarID, Value] of Object.entries(Vars)) {
+		if (!List.includes(VarID)) NDT.Spr.Var.Create('NDT', VarID);
+		if (NDT.Spr.Var.Get('NDT', VarID) !== Value) NDT.Spr.Var.Set('NDT', VarID, Value);
+	}
+}
+
+NDT.SC.Step = NDT.RT._step;
+NDT.RT._step = function() {
+    NDTValiable();
+    NDT.NDTEvent.Dispatch('STEP_BEFORE');
+    NDT.SC.Step.call(this);
+    NDT.NDTEvent.Dispatch('STEP_AFTER');
+}
+
+NDT.SC.StartHats = NDT.RT.startHats;
+NDT.RT.startHats = function(HatOpc, Option, Target) {
+    if (NDT.Option.DisNDTEvent) return NDT.SC.StartHats.call(this, HatOpc, Option, Target);
+    const HatID = HatOpc.toUpperCase();
+    const SprID = Target?.id || 'ALL';
+    const Mes = (HatID == 'EVENT_WHENBROADCASTRECEIVED');
+    const Flag = (HatID == 'EVENT_WHENFLAGCLICKED');
+    const Eve = NDT.NDTEvent.Dispatch;
+    if (Mes) Eve('MESSAGE_BEFORE', {MesID: Option.BROADCAST_OPTION, SprID: SprID})
+    if (Flag) Eve('FLAG_BEFORE', {Option: Option, SprID: SprID})
+    const Res = NDT.SC.StartHats.call(this, HatOpc, Option, Target);
+    if (Mes) Eve('MESSAGE_AFTER', {MesID: Option.BROADCAST_OPTION, SprID: SprID})
+    if (Flag) Eve('FLAG_AFTER', {Option: Option, SprID: SprID})
+    return Res;
+}
+
+// NDTMain
+// Event
+NDT.Event.Flag = function() {
+    NDT.VM.greenFlag();
+}
+NDT.Event.Stop = function() {
+    NDT.VM.stopAll();
+}
+NDT.Event.Message = function(Message) {
+    ChkType('s', Message);
+    startHats("event_whenbroadcastreceived", { BROADCAST_OPTION: Message} );
+}
+
+// Sprite
+NDT.Spr.Get = function(SprID) {
+    ChkType('s', SprID);
+    const Sprites = NDT.Spr.All;
+    let Out = Sprites.find(s => s.id == SprID);
+    if (!Out) {
+        Out = Sprites.find(s => s.getName() == SprID);
+        if (!Out) {
+            Log('e', `${SprID}というスプライトは見つかりませんでした`);
+            return;
+        }
+    }
+    return Out;
+}
+NDT.Spr.Runtime = function(SprID) {
+    return NDT.Spr.Get(SprID).runtime;
+}
+NDT.Spr.RT = function(SprID) {
+    return NDT.Spr.Runtime(SprID);
+}
+NDT.Spr.Add = async function(URL) {
+    ChkType('s', URL);
+    const res = await fetch(URL);
+    const json = await res.arrayBuffer();
+    try {
+        await NDT.VM.addSprite(json);
+    } catch (e) {
+        console.error(e);
+    }
+}
+NDT.Spr.Upload = async function() {
+    NDT.Spr.Add(await FileUpload('.sprite3'));
+}
+NDT.Spr.Delete = function(SprID) {
+    ChkType('s', SprID);
+    const Id = NDT.Spr.Get(SprID).id;
+    NDT.VM.deleteSprite(SprID);
+}
+NDT.Spr.Rename = function(SprID, NewName) {
+    ChkType('s', SprID);
+    ChkType('s', NewName);
+    const Id = NDT.Spr.Get(SprID).id;
+    if (!Id) return;
+    const Pos = NDT.Spr.IDList.indexOf(Id);
+    NDT.Spr.All[Pos].sprite.name = NewName;
+}
+NDT.Spr.Visible = function(SprID, Show = null) {
+    const Spr = NDT.Spr.Get(SprID);
+    if (!Spr) return;
+    if (Show !== null) {
+        Spr.visible = Show;
+    }
+    return Spr.visible;
+},
+NDT.Spr.Size = function(SprID, ToSize = null) {
+    const Spr = NDT.Spr.Get(SprID);
+    if (!Spr) return;
+    if (ToSize !== null) {
+        Spr.size = ToSize;
+    }
+    return Spr.size;
+}
+
+NDT.Spr.Pos.Get = function(SprID) {
+    const Spr = NDT.Spr.Get(SprID);
+    if (!Spr) return;
+    return { x: Spr.x, y: Spr.y, Dir: Spr.direction };
+}
+NDT.Spr.Pos.Goto = function(SprID, ToX = null, ToY = null) {
+    const Spr = NDT.Spr.Get(SprID);
+    if (!Spr) return;
+    if (ToX !== null) Spr.x = ToX;
+    if (ToY !== null) Spr.y = ToY;
+    return { x: Spr.x, y: Spr.y };
+}
+NDT.Spr.Pos.MoveXY = function(SprID, StepX = null, StepY = null) {
+    const Spr = NDT.Spr.Get(SprID);
+    if (!Spr) return;
+    if (StepX !== null) Spr.x += StepX;
+    if (StepY !== null) Spr.y += StepY;
+    return { x: Spr.x, y: Spr.y };
+}
+NDT.Spr.Pos.Move = function(SprID, Steps) {
+    const Spr = NDT.Spr.Get(SprID);
+    if (!Spr) return;
+    const Radians = Math.PI / 180 * (90 - Spr.direction);
+    const StepX = Steps * Math.cos(Radians);
+    const StepY = Steps * Math.sin(Radians);
+    Spr.x += StepX;
+    Spr.y += StepY;
+    return { x: Spr.x, y: Spr.y };
+}
+NDT.Spr.Pos.SetDir = function(SprID, Dir = null) {
+    const Spr = NDT.Spr.Get(SprID);
+    if (!Spr) return;
+    if (Dir !== null) Spr.direction = Dir;
+    return { Dir: direction };
+}
+NDT.Spr.Pos.Turn = function(SprID, Dir) {
+    const Spr = NDT.Spr.Get(SprID);
+    if (!Spr) return;
+    if (Dir) Spr.direction += Dir;
+    return { Dir: Spr.direction };
+}
+
+NDT.Spr.Eve.Flag = function(SprID) {
+    ChkType('s', SprID);
+    const target = NDT.Spr.Get(SprID);
+    if (!target) return;
+    NDT.RT.startHats('event_whenflagclicked', {}, target);
+}
+NDT.Spr.Eve.Stop = function(SprID) {
+    ChkType('s', SprID);
+    const target = NDT.Spr.Get(SprID);
+    if (!target) return;
+    NDT.RT.stopForTarget(target);
+}
+NDT.Spr.Eve.Message = function(SprID, Message) {
+    ChkType('s', SprID);
+    ChkType('s', Message);
+    const target = NDT.Spr.Get(SprID);
+    if (!target) return;
+    NDT.RT.startHats('event_whenbroadcastreceived', { BROADCAST_OPTION: Message }, target);
+}
+
+NDT.Spr.Var.All = function(SprID) {
+    const target = NDT.Spr.Get(SprID);
+    if (!target) return;
+    return Object.values(target.variables).filter(v => v.type == '');
+}
+NDT.Spr.Var.IDList = function(SprID) {
+    return NDT.Spr.Var.All(SprID).map(v => v.id);
+}
+NDT.Spr.Var.NameList = function(SprID) {
+    return NDT.Spr.Var.All(SprID).map(v => v.name);
+}
+NDT.Spr.Var.GetFull = function(SprID, VarID) {
+    ChkType('s', SprID);
+    ChkType('s', VarID);
+    const target = NDT.Spr.Get(SprID);
+    if (!target) return;
+    const Vars = Object.values(target.variables);
+    let Out = Vars.find(v => v.id === VarID && v.type == '');
+    if (!Out) {
+        Out = Vars.find(v => v.name === VarID && v.type == '');
+        if (!Out) {
+            Log('e', `スプライト${SprID}に${VarID}というローカル変数は見つかりませんでした`);
+            return;
+        }
+    }
+    return Out;
+}
+NDT.Spr.Var.Get = function(SprID, VarID) {
+    return NDT.Spr.Var.GetFull(SprID, VarID,).value;
+}
+NDT.Spr.Var.Set = function(SprID, VarID, Value) {
+    NDT.Spr.Var.GetFull(SprID, VarID).value = Value;
+}
+NDT.Spr.Var.Change = function(SprID, VarID, Value) {
+    NDT.Spr.Var.GetFull(SprID, VarID).value += Value;
+}
+NDT.Spr.Var.Rename = function(SprID, VarID, NewName) {
+    NDT.Spr.Var.GetFull(SprID, VarID).name = NewName;
+}
+NDT.Spr.Var.Create = function(SprID, VarName) {
+    const UID = GenerateUid();
+    NDT.Spr.Get(SprID).createVariable(UID, VarName, '');
+}
+NDT.Spr.Var.Delete = function(SprID, VarID) {
+    const ID = NDT.Spr.Var.GetFull(SprID, VarID).id;
+    NDT.Spr.Get(SprID).deleteVariable(ID);
+}
+
+NDT.Spr.List.All = function(SprID) {
+    const target = NDT.Spr.Get(SprID);
+    if (!target) return;
+    return Object.values(target.variables).filter(v => v.type == 'list');
+}
+NDT.Spr.List.IDList = function(SprID) {
+    return NDT.Spr.List.All(SprID).map(v => v.id);
+}
+NDT.Spr.List.NameList = function(SprID) {
+    return NDT.Spr.List.All(SprID).map(v => v.name);
+}
+NDT.Spr.List.GetFull = function(SprID, VarID) {
+    ChkType('s', SprID);
+    ChkType('s', VarID);
+    const target = NDT.Spr.Get(SprID);
+    if (!target) return;
+    const Vars = Object.values(target.variables);
+    let Out = Vars.find(v => v.id === VarID && v.type == 'list');
+    if (!Out) {
+        Out = Vars.find(v => v.name === VarID && v.type == 'list');
+        if (!Out) {
+            Log('e', `スプライト${SprID}に${VarID}というローカルリストは見つかりませんでした`);
+            return;
+        }
+    }
+    return Out;
+}
+NDT.Spr.List.Get = function(SprID, VarID) {
+    return NDT.Spr.List.GetFull(SprID, VarID,).value;
+}
+NDT.Spr.List.SetArray = function(SprID, VarID, Value) {
+    const List = NDT.Spr.List.Get(SprID, VarID);
+    List.length = 0;
+    List.push(...Value);
+}
+NDT.Spr.List.Create = function(SprID, VarName) {
+    const UID = GenerateUid();
+    NDT.Spr.Get(SprID).createVariable(UID, VarName, 'list');
+}
+NDT.Spr.List.Delete = function(SprID, VarID) {
+    const ID = NDT.Spr.List.GetFull(SprID, VarID).id;
+    NDT.Spr.Get(SprID).deleteVariable(ID);
+}
+NDT.Spr.List.Rename = function(SprID, VarID, NewName) {
+    NDT.Spr.List.GetFull(SprID, VarID).name = NewName;
+}
+
+
+// Variable
+NDT.Var.All = function() {
+    const SprID = NDT.Spr.All.find(s => s.isStage).id;
+    if (!SprID) {
+        Log('e', 'ステージを発見できませんでした');
+        return;
+    }
+    return NDT.Spr.Var.All(SprID);
+}
+NDT.Var.IDList = function() {
+    return NDT.Var.All().map(v => v.id);
+}
+NDT.Var.NameList = function() {
+    return NDT.Var.All().map(v => v.name);
+}
+NDT.Var.GetFull = function(VarID) {
+    ChkType('s', VarID);
+    const target = NDT.Spr.All.find(s => s.isStage);
+    if (!target) {
+        Log('e', 'ステージを発見できませんでした');
+        return;
+    }
+    const Vars = Object.values(target.variables);
+    let Out = Vars.find(v => v.id === VarID && v.type == '');
+    if (!Out) {
+        Out = Vars.find(v => v.name === VarID && v.type == '');
+        if (!Out) {
+            Log('e', `${VarID}というグローバル変数は見つかりませんでした`);
+            return;
+        }
+    }
+    return Out;
+}
+NDT.Var.Get = function(VarID) {
+    return NDT.Var.GetFull(VarID).value;
+}
+NDT.Var.Set = function(VarID, Value){
+    NDT.Var.GetFull(VarID).value = Value;
+}
+NDT.Var.Change = function(VarID, Value){
+    NDT.Var.GetFull(VarID).value += Value;
+}
+NDT.Var.Create = function(VarName) {
+    NDT.RT.createNewGlobalVariable(VarName);
+}
+NDT.Var.Delete = function(VarID) {
+    ChkType('s', VarID);
+    const SprID = NDT.Spr.All.find(s => s.isStage).id;
+    if (!SprID) {
+        Log('e', 'ステージを発見できませんでした');
+        return;
+    }
+    NDT.Spr.Get(SprID).deleteVariable(NDT.Var.GetFull(VarID).id);
+}
+NDT.Var.Rename = function(VarID, NewName){
+    NDT.Var.GetFull(VarID).name = NewName;
+}
+
+
+// List
+NDT.List.All = function() {
+    const SprID = NDT.Spr.All.find(s => s.isStage).id;
+    if (!SprID) {
+        Log('e', 'ステージを発見できませんでした');
+        return;
+    }
+    return NDT.Spr.List.All(SprID);
+}
+NDT.List.IDList = function() {
+    return NDT.List.All().map(v => v.id);
+}
+NDT.List.NameList = function() {
+    return NDT.List.All().map(v => v.name);
+}
+NDT.List.GetFull = function(VarID) {
+    ChkType('s', VarID);
+    const target = NDT.Spr.All.find(s => s.isStage);
+    if (!target) {
+        Log('e', 'ステージを発見できませんでした');
+        return;
+    }
+    const Vars = Object.values(target.variables);
+    let Out = Vars.find(v => v.id === VarID && v.type == 'list');
+    if (!Out) {
+        Out = Vars.find(v => v.name === VarID && v.type == 'list');
+        if (!Out) {
+            Log('e', `${VarID}というグローバルリストは見つかりませんでした`);
+            return;
+        }
+    }
+    return Out;
+}
+NDT.List.Get = function(VarID) {
+    return NDT.List.GetFull(VarID).value;
+}
+NDT.List.SetArray = function(VarID, Value) {
+    const List = NDT.List.Get(VarID);
+    List.length = 0;
+    List.push(...Value);
+}
+NDT.List.Create = function(VarName) {
+    NDT.RT.createNewGlobalVariable(VarName, undefined, 'list');
+}
+NDT.List.Delete = function(VarID) {
+    ChkType('s', VarID);
+    const SprID = NDT.Spr.All.find(s => s.isStage).id;
+    if (!SprID) {
+        Log('e', 'ステージを発見できませんでした');
+        return;
+    }
+    NDT.Spr.Get(SprID).deleteVariable(NDT.List.GetFull(VarID).id);
+}
+NDT.List.Rename = function(VarID, NewName){
+    NDT.List.GetFull(VarID).name = NewName;
+}
+
+
+// 色々使う関数
+// 短縮表現変換
+function Abbreviation(code, ...link) {
+    for (const word of link) {
+        if (code.toLowerCase().startsWith(word.toLowerCase()[0])) {
+            return word;
+        }
+    }
+    Log('w', `引数として想定されていない値が入力されました: ${code}`)
+    return code;
+}
+// ログ
+function Log(type = 'log', output) {
+    const lstype = Abbreviation(
+        type,
+        'log',
+        'warn',
+        'error'
+    )
+    console[lstype](`[NDT] ${output}`)
+}
+// 型チェック
+function ChkType(type, data) {
+    if (NDT.Option.DisCheck) return;
+    const lstype = Abbreviation(
+        type,
+        'Number',
+        'String',
+        'Symbol',
+        'Boolean',
+        'BigInt',
+        'Undefined',
+        'Null',
+        'Object',
+        'Function'
+    )
+    if ((typeof data).toLowerCase() !== lstype.toLowerCase()) {
+        Log('e', `引数に指定できない型が指定されています!:\n入力=>${typeof data} 要求=>${lstype}`)
+    }
+}
+// ファイルのアップロード
+async function FileUpload(...exts) {
+	const [handle] = await window.showOpenFilePicker({
+		types: [{
+			accept: {
+				"*/*": exts
+			}
+		}]
+	});
+	const file = await handle.getFile();
+	const reader = new FileReader();
+	return new Promise(resolve => {
+		reader.onload = e => resolve(e.target.result);
+		reader.readAsDataURL(file)
+	})
+}
+// ランダムなIDの生成
+function GenerateUid() {
+    return Math.random().toString(36).substring(2, 12) +
+        Math.random().toString(36).substring(2, 12);
+};
+
+
+// イベント
+document.dispatchEvent(new Event("NDT_Loaded"));
+Log('l', `NDT V${NDT.Info.Ver} is Loaded.`);
+Log('l', `Update:\n${NDT.Info.Message}`)
+})();
--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# NDT
-ScratchVMにある程度簡単にアクセスできるようになる...と思うスクリプト
+**注意: これはTurboWarpのカスタム拡張機能ではありません**  
+# NekoDevTools
+NekoDevTools(NDT)はScratchVMにある程度簡単にアクセスできるようになる何かです。  
+バニラでも**多分**使えるしTurboWarpのようなMod環境でも**多分**使えます。  
+コンソールなどでNDTを実行後``NDT``でアクセスできます。  
+詳しい使い方はやる気が出たら書きます。
+# 使い方
+下のコードをブックマークして実行するかコンソールから直接実行するとNDTが壊れてなければ起動メッセージが出て起動します。
+```js
+javascript:(function(){if(window.NDT){return} const s=document.createElement('script');s.src='https://nyantorusabu.github.io/NDT/NekoDevTools.js';document.body.appendChild(s)})();
+```  
+起動しなかったらぁ...Issue立てといてください。  
+Issueの立て方わからんかったらScratchかDiscordで伝えてください。
+# 現在使えるもの
+- ``NDT.VM`` - ScratchVMに直接アクセスできます。
+- ``NDT.RT`` - Runtimeにアクセスできます。ただのショートカット。
+- ``NDT.Event`` or ``NDT.Eve`` - イベント関係。
+- ``NDT.Sprite`` or ``NDT.Spr`` - スプライト関係。
+- ``NDT.Sprite.Variable`` or ``NDT.Sprite.Var`` - ローカル変数関係。
+- ``NDT.Variable`` or ``NDT.Var`` - グローバル変数関係。
+- ``NDT.NDTEvent`` or ``NDT.NEve`` - NDTのイベント関係
+- ``NDT.Option`` - 設定
+- ``NDT.Info`` - NDTの情報
+# 設定できる項目
+- ``NDT.Option.DisCheck`` - Boolean(真偽値) - 有効にすると実行時型チェックを行わないようになります。軽量化に。
+- ``NDT.Option.DisNDTEvent`` - Boolean(真偽値) - 有効にするとNDTEventのイベント送信がなくなります。こちらも軽量化に。
+# 型チェックについて
+NDTは関数を実行するときに一部の引数の型を確認します。  
+型が間違っていた場合エラーを吐きます(処理は止めません)  
+無効にしたい場合は``NDT.Option.DisCheck``を有効にしてください。
+# NDTEventについて
+NDTはScratchのイベントに加えて色々独自のイベントがあります。  
+``NDT.NDTEvent.Add(name, handler)``でイベントを受け取れます。  
+無効にしたい場合は``NDT.Option.DisNDTEvent``を有効にしてください。  
+使い方の例とか無いんでここには一覧だけ置いときます。  
+- ``STEP_BEFORE`` - Stepの前に発火します
+- ``STEP_AFTER`` - Stepの後に発火します
+- ``FLAG_BEFORE`` - 緑の旗が押される前に発火します
+- ``FLAG_AFTER`` - 緑の旗が押された後に発火します
+- ``MESSAGE_BEFORE`` - 何らかのメッセージが送信される前に発火します
+- ``MESSAGE_AFTER`` - 何らかのメッセージが送信された後に発火します
+# サンプル
+1. ``NDT.Eve.Flag()`` - 緑の旗を押す
+2. ``NDT.Eve.Stop()`` - すべて停止
+3. ``NDT.Spr.Rename(NDT.Var.Get('hoge'), NDT.Var.Get('hogege'))`` - スプライト名がグローバル変数"hoge"内の値になっているスプライトの名前をグローバル変数"hogege"内の値にする
+# ライセンス
+NDTはMITライセンスです。改変、配布などなど自由!
